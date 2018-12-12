b3d846d85a395951f54a63390fab87c09c57df8e
ronnie <ronnie@ronnie.local>
2018-12-12 14:43:04 -0500

    word bank created and return method made

--- hangman.py
+++ hangman.py
@@ -1,3 +1,6 @@
  #import random for choosing word
 import random 
 def chooseWord():
+	 # my list of words
+    word_list = ["ronnie","spiraling","stratosphere","ligma","wig","peppermint","furries","informatics","hello","nyoom","oatmeal","roddent"]
+    return random.choice(word_list) #return a random selection from the list
