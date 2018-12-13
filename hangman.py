f32b5e88c2b3e4bc0a62b348810309961c310ffb
ronnie <ronnie@ronnie.local>
2018-12-12 21:44:09 -0500

    Print function

--- hangman.py
+++ hangman.py
@@ -4,3 +4,8 @@ def chooseWord():
 	 # my list of words
     word_list = ["ronnie","spiraling","stratosphere","ligma","wig","peppermint","furries","informatics","hello","nyoom","oatmeal","roddent"]
     return random.choice(word_list) #return a random selection from the list
+
+def printObscured(obscured):
+    for letter in obscured: #for each character in the obscured string
+        print(letter+" ",end="") #print the character and a space
+    print()    
