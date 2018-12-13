9703712067f869e55d9fc0f83eec2fbce3125833
ronnie <ronnie@ronnie.local>
2018-12-12 22:27:41 -0500

    Input function declaration

--- hangman.py
+++ hangman.py
@@ -9,3 +9,6 @@ def printObscured(obscured):
     for letter in obscured: #for each character in the obscured string
         print(letter+" ",end="") #print the character and a space
     print()    
+    
+def validInput(user_input):
+    return len(user_input) == 1 and str.isalpha(user_input) #if the user input is of length 1 and is alphabetic
