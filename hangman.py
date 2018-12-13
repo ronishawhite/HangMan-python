286a3ad75f03b385b1b304331751338d2472e5dc
ronnie <ronnie@ronnie.local>
2018-12-12 22:37:41 -0500

    main function setup

--- hangman.py
+++ hangman.py
@@ -12,3 +12,11 @@ def printObscured(obscured):
     
 def validInput(user_input):
     return len(user_input) == 1 and str.isalpha(user_input) #if the user input is of length 1 and is alphabetic
+
+
+def main():
+    word = chooseWord() #choose a word
+    obscured = "_"*len(word) #create the obscured string based on the length of the word
+    gameOver = False #game initially not over
+    success = False #assume game was not successful
+    guesses = 0 #intitially 0 guesses
