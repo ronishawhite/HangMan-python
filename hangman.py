01810943d2b8a2dd33a5564aa884d6563cb6229e
ronnie <ronnie@ronnie.local>
2018-12-16 15:42:05 -0500

    changed word bank in more appropiate terms fixed main function to run program

--- hangman.py
+++ hangman.py
@@ -2,7 +2,7 @@
 import random 
 def chooseWord():
 	 # my list of words
-    word_list = ["ronnie","spiraling","stratosphere","ligma","wig","peppermint","furries","informatics","hello","nyoom","oatmeal","roddent"]
+    word_list = ["informatics","hangman","stratosphere","muji","tea","peppermint","furries","strange","hello","wig","oatmeal","roddent"]
     return random.choice(word_list) #return a random selection from the list
 
 def printObscured(obscured):
@@ -55,4 +55,7 @@ def main():
         print("Word solved in",guesses,"guesses") #report guesses
     else:
         print("Max guesses reached. Word was:",word) #tell user answer
+
+
+main()
     
